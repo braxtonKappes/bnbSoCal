c5609a11a22c84a6b438f3f0b67bc068d6b35993
Braxton Kappes <braxtonkappes@gmail.com>
2022-03-06 22:26:39 -0600

    heroku scripts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
@@ -14,21 +8,21 @@ router.use('/api', apiRouter);
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
-    // Serve the frontend's index.html file at the root route
+  // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
         path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
 });
 
-    // Serve the static assets in the frontend's build folder
+  // Serve the static assets in the frontend's build folder
     router.use(express.static(path.resolve("../frontend/build")));
 
-    // Serve the frontend's index.html file at all other routes NOT starting with /api
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
         path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -37,8 +31,8 @@ if (process.env.NODE_ENV === 'production') {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
     });
 }
 
